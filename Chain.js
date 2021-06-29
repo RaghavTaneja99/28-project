5d22001d41e2d059f770f6aec4c0dd14d549092e
RaghavTaneja99 <78413994+RaghavTaneja99@users.noreply.github.com>
2021-06-29 14:49:29 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,32 @@
+class Chain{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.bodyA=bodyA
+        this.pointB=pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+    attach(){
+        this.launcher.bodyA=this.bodyA
+    }
+    fly(){
+        this.launcher.bodyA=null
+    }
+ 
+
+    display(){
+        if(this.launcher.bodyA)
+        {var pointA = this.launcher.bodyA.position;
+        
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }}
+    
+    
+}
